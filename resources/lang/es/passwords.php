f5c7e3a74b515b04d1d8984118347d177444d913
cotelus <cotelus@gmail.com>
2021-02-23 20:36:43 +0000

    idiomas bien

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido modificada!',
+    'sent' => 'Te hemos enviado un email con el link!',
+    'throttled' => 'Por favor espere mientras lo intentamos de nuevo.',
+    'token' => 'Token de cambio de contraseña incorrecto.',
+    'user' => "No se encuentra un usuario con es dirección de correo.",
+
+];
