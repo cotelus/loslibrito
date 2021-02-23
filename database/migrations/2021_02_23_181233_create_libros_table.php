cdfbbac6b286f4cc347183c09a1210ad9c3542ff
cotelus <cotelus@gmail.com>
2021-02-23 18:25:37 +0000

    todo rula

---
+++ database/migrations/2021_02_23_181233_create_libros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('anio', 8);
+            $table->string('escritor', 64);
+            $table->string('portada');
+            $table->boolean('disponible')->default(true);
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
